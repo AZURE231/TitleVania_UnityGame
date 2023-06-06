10e0e301bf82233b82b8e192e44546a81a411f3c
AZURE231 <95262095+AZURE231@users.noreply.github.com>
2023-06-06 17:00:31 +0700

    Change level

--- Assets/Scripts/LevelExit.cs
+++ Assets/Scripts/LevelExit.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelExit : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] float levelLoadDelay = 1f;
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        StartCoroutine(LoadNextLevel());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator LoadNextLevel()
     {
-        
+        yield return new WaitForSecondsRealtime(levelLoadDelay);
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene((currentSceneIndex + 1) % 3);
     }
 }
